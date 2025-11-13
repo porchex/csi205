f04ea345c55933eb771fd357e3601ed5febeaaa3
porchex <thanachon.che@spumail.net>
2025-11-13 23:33:35 +0700

    commit

---
+++ src/components/AppFooter.jsx
@@ -0,0 +1,16 @@
+import fblogo from "/images/FBLOGO.png";
+import iglogo from "/images/IG.jpg"; 
+
+const AppFooter = () => {
+    return ( 
+    <>
+        <h3 style={{ textAlign: 'center'}}>SPU-SIT-CSI</h3>
+        <div>
+        <a href="https://www.instagram.com/porrxz_/?hl=th "  target="_blank"><img src={iglogo} style={{ width: '40px', height: '40px' }}/></a>
+        <a href="https://www.facebook.com/thanachon.chaengcharoen "  target="_blank"><img src={fblogo} style={{ width: '30px', height: '30px' }}/></a>
+        </div>
+    </> 
+    );
+}
+ 
+export default AppFooter;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,53 @@
+import { useRef } from "react";
+import Form from "react-bootstrap/Form";
+
+import "./Login.css";
+import { verifyUser } from "../data/users";
+
+function Login( {setToken, setRole}) {
+    const userRef = useRef()
+    const passRef = useRef()
+
+  return (
+    <div className="login-container">
+      <Form.Label htmlFor="username">Username</Form.Label>
+      <Form.Control
+        type="text"
+        id="username"
+        placeholder="user"
+        title="type : user"
+        style={{textAlign:'center'}}
+        ref={userRef}
+      />
+       <Form.Label htmlFor="password">Password</Form.Label>
+      <Form.Control
+        type="password"
+        id="password"
+        placeholder="pass"
+        title="type : pass"
+        style={{textAlign:'center'}}
+        ref={passRef}
+        
+      />
+      <button className= 'btn btn-success mt-3' onClick={() => {
+        const user = userRef.current.value.trim ()
+        const pass = passRef.current.value.trim ()
+        userRef.current.value = ''
+        passRef.current.value = ''
+        const userInfo = verifyUser(user, pass)
+
+        if (userInfo === null){
+            alert ('Wrong username or password')
+            userRef.current.focus()
+        }else {
+            setToken(userInfo.token)
+            setRole (userInfo.role)
+        }
+
+
+      } }>Login</button>
+    </div>
+  );
+}
+
+export default Login;
