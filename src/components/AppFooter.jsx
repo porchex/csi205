e5db85e9a3e6bcef41fe0b0db2e7b8292846ee8c
porchex <thanachon.che@spumail.net>
2025-11-13 21:23:24 +0700

    re commit

--- src/components/AppFooter.jsx
+++ src/components/AppFooter.jsx
@@ -6,8 +6,8 @@ const AppFooter = () => {
     <>
         <h3 style={{ textAlign: 'center'}}>SPU-SIT-CSI</h3>
         <div>
-        <a href="https://www.instagram.com/porrxz_/?hl=th "  target="_blank"><img src={fblogo} style={{ width: '40px', height: '40px' }}/></a>
-        <a href="https://www.facebook.com/thanachon.chaengcharoen "  target="_blank"><img src={iglogo} style={{ width: '30px', height: '30px' }}/></a>
+        <a href="https://www.instagram.com/porrxz_/?hl=th "  target="_blank"><img src={iglogo} style={{ width: '40px', height: '40px' }}/></a>
+        <a href="https://www.facebook.com/thanachon.chaengcharoen "  target="_blank"><img src={fblogo} style={{ width: '30px', height: '30px' }}/></a>
         </div>
     </> 
     );
