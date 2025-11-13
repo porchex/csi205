2ad7e177a39030e460bb47aca359d1a57e4e02bb
porchex <thanachon.che@spumail.net>
2025-11-13 21:53:16 +0700

    re commit

--- src/pages/Animation.jsx
+++ src/pages/Animation.jsx
@@ -59,7 +59,7 @@ export default function App() {
       backgroundImage: `url(${images[cur]})`,
     });
   };
-
+        //  ------------------------------
   const step = () => {
     setX((prevX) => {
       let nextX = prevX + (goRight.current ? vx.current : -vx.current);
@@ -90,7 +90,7 @@ export default function App() {
     backgroundSize: "cover",
     position: "relative",
     borderRadius: "15px",
-    boxShadow: "0 0 10px rgba(0,0,0,0.2)" ,
+    boxShadow: "0 0 10px rgba(0,0,0,0.2)",
     overflow: "hidden",
   };
 
