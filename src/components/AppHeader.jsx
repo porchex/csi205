22d6d647a69ac408e8499d59ca2ec9f3f8fc9410
porchex <thanachon.che@spumail.net>
2025-11-13 22:08:26 +0700

    recommit

--- src/components/AppHeader.jsx
+++ src/components/AppHeader.jsx
@@ -1,17 +1,16 @@
-import "./AddHeader.css"
+import "./AddHeader.css";
 import logo from "/images/logo.png";
+
 const AppHeader = () => {
+  return (
+    <>
+      <div>
+        <img src={logo} className="AppHeader" />
+      </div>
 
-    return ( 
-        <>
-            <div>
-                <img src={logo} className="AppHeader" />
-            </div>
+      <h1 style={{ textAlign: 'center' }}>CSI205 การพัฒนาโปรแกรมส่วนหน้า</h1>
+    </>
+  );
+};
 
-
-            <h1 style={{ textAlign: 'center' }}>CSI205 การพัฒนาโปรแกรมส่วนหน้า</h1>
-        </> 
-    );
-}
- 
 export default AppHeader;
